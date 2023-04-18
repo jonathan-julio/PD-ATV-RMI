8f334fcd7fbe1246e47c4ab54df434fdbcab7187
jonathan-julio <55097116+jonathan-julio@users.noreply.github.com>
2023-04-18 20:49:35 -0300

    Update AmbulanceService.java

--- hospital/src/AmbulanceService.java
+++ hospital/src/AmbulanceService.java
@@ -2,5 +2,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface AmbulanceService extends Remote {
-    public Hospital findNearestHospital(Location location) throws RemoteException;
+    Hospital findNearestHospital(double latitude, double longitude) throws RemoteException;
 }
