0b4ed832ea53c68df15083da82362a59753f6b38
jonathan-julio <55097116+jonathan-julio@users.noreply.github.com>
2023-04-18 20:49:17 -0300

    Update HospitalImpl.java

--- hospital/src/HospitalImpl.java
+++ hospital/src/HospitalImpl.java
@@ -2,19 +2,32 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class HospitalImpl extends UnicastRemoteObject implements Hospital {
-    private String name;
-    private Location location;
 
-    public HospitalImpl(String name, Location location) throws RemoteException {
+    private String name;
+    private String address;
+    private double latitude;
+    private double longitude;
+
+    public HospitalImpl(String name, String address, double latitude, double longitude) throws RemoteException {
         this.name = name;
-        this.location = location;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public String getName() throws RemoteException {
         return name;
     }
 
-    public Location getLocation() throws RemoteException {
-        return location;
+    public String getAddress() throws RemoteException {
+        return address;
+    }
+
+    public double getLatitude() throws RemoteException {
+        return latitude;
+    }
+
+    public double getLongitude() throws RemoteException {
+        return longitude;
     }
 }
