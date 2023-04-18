21aa92e99266839435917a5cc6a9652ae95c1270
jonathan-julio <55097116+jonathan-julio@users.noreply.github.com>
2023-04-18 20:40:25 -0300

    Update AmbulanceServiceImpl.java

--- hospital/src/AmbulanceServiceImpl.java
+++ hospital/src/AmbulanceServiceImpl.java
@@ -4,28 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AmbulanceServiceImpl extends UnicastRemoteObject implements AmbulanceService {
-
     private List<Hospital> hospitals;
 
-    public AmbulanceServiceImpl() throws RemoteException {
-        super();
-        // Configura a lista de hospitais disponíveis
-        hospitals = new ArrayList<>();
-        hospitals.add(new Hospital("Hospital A", -23.5505, -46.6333));
-        hospitals.add(new Hospital("Hospital B", -23.5506, -46.6334));
-        hospitals.add(new Hospital("Hospital C", -23.5507, -46.6335));
+    public AmbulanceServiceImpl(List<Hospital> hospitals) throws RemoteException {
+        this.hospitals = hospitals;
     }
 
-    @Override
-    public Hospital getNearestHospital(double latitude, double longitude) throws RemoteException {
-        double minDistance = Double.MAX_VALUE;
+    public Hospital findNearestHospital(Location location) throws RemoteException {
         Hospital nearestHospital = null;
+        double nearestDistance = Double.MAX_VALUE;
 
-        // Itera sobre a lista de hospitais disponíveis para encontrar o mais próximo
         for (Hospital hospital : hospitals) {
-            double distance = calculateDistance(latitude, longitude, hospital.getLatitude(), hospital.getLongitude());
-            if (distance < minDistance) {
-                minDistance = distance;
+            Location hospitalLocation = hospital.getLocation();
+            double distance = calculateDistance(location, hospitalLocation);
+
+            if (distance < nearestDistance) {
+                nearestDistance = distance;
                 nearestHospital = hospital;
             }
         }
@@ -33,19 +27,23 @@ public class AmbulanceServiceImpl extends UnicastRemoteObject implements Ambulan
         return nearestHospital;
     }
 
-    // Calcula a distância entre dois pontos em uma esfera (utilizando a fórmula de Haversine)
-    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-        int earthRadiusKm = 6371;
+    private double calculateDistance(Location location1, Location location2) {
+        double lat1 = location1.getLatitude();
+        double lon1 = location1.getLongitude();
+        double lat2 = location2.getLatitude();
+        double lon2 = location2.getLongitude();
 
+        double R = 6371; // raio da terra em km
         double dLat = Math.toRadians(lat2 - lat1);
         double dLon = Math.toRadians(lon2 - lon1);
+        double lat1Radians = Math.toRadians(lat1);
+        double lat2Radians = Math.toRadians(lat2);
 
-        lat1 = Math.toRadians(lat1);
-        lat2 = Math.toRadians(lat2);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1Radians) * Math.cos(lat2Radians);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double distance = R * c;
 
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                   Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return earthRadiusKm * c;
+        return distance;
     }
 }
