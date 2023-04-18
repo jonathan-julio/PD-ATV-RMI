a8bfeb1fd60f18f312ad7f7f32cf3e3060ce11a8
jonathan-julio <55097116+jonathan-julio@users.noreply.github.com>
2023-04-18 20:41:25 -0300

    Update HospitalLocator.java

--- hospital/src/HospitalLocator.java
+++ hospital/src/HospitalLocator.java
@@ -1,29 +1,8 @@
-import java.rmi.Naming;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.server.ExportException;
+import java.util.List;
 
-public class HospitalLocator {
-
-    public static void main(String[] args) {
-        try {
-            // Cria o registro RMI na porta 1099
-            LocateRegistry.createRegistry(1099);
-
-            // Cria a instância do serviço de solicitação de ambulância
-            AmbulanceService ambulanceService = new AmbulanceServiceImpl();
-
-            // Publica o serviço de solicitação de ambulância com o nome "AmbulanceService"
-            Naming.rebind("AmbulanceService", ambulanceService);
-
-            System.out.println("HospitalLocator is running...");
-        } catch (ExportException e) {
-            System.out.println("The RMI Registry is already running.");
-        } catch (RemoteException e) {
-            System.out.println("Error starting the RMI registry: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Error binding the AmbulanceService: " + e.getMessage());
-        }
-    }
-
+public interface HospitalLocator extends Remote {
+    List<Hospital> getAvailableHospitals() throws RemoteException;
+    Hospital findNearestHospital(Location location) throws RemoteException;
 }
