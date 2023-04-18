86563c64f460dcd2a1a8dd135824874bb5010a31
jonathan-julio <55097116+jonathan-julio@users.noreply.github.com>
2023-04-18 20:39:11 -0300

    Create Location.java

---
+++ hospital/src/Location.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class Location implements Serializable {
+    private double latitude;
+    private double longitude;
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
