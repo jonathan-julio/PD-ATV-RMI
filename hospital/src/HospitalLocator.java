7fb9a29e14849547767ba2c52ba18c7e59b2be47
jonathan-julio <55097116+jonathan-julio@users.noreply.github.com>
2023-04-18 20:50:34 -0300

    Update HospitalLocator.java

--- hospital/src/HospitalLocator.java
+++ hospital/src/HospitalLocator.java
@@ -1,8 +1,31 @@
-import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.List;
 
-public interface HospitalLocator extends Remote {
-    List<Hospital> getAvailableHospitals() throws RemoteException;
-    Hospital findNearestHospital(Location location) throws RemoteException;
+public class HospitalLocator extends UnicastRemoteObject implements HospitalLocatorInterface {
+    private static final long serialVersionUID = 1L;
+    private List<Hospital> hospitals;
+
+    public HospitalLocator() throws RemoteException {
+        super();
+        hospitals = new ArrayList<>();
+        hospitals.add(new Hospital("Hospital A", new Location(10, 10)));
+        hospitals.add(new Hospital("Hospital B", new Location(20, 20)));
+        hospitals.add(new Hospital("Hospital C", new Location(30, 30)));
+    }
+
+    @Override
+    public Hospital findNearestHospital(Location location) throws RemoteException {
+        Hospital nearestHospital = null;
+        double nearestDistance = Double.MAX_VALUE;
+        for (Hospital hospital : hospitals) {
+            double distance = hospital.getLocation().distance(location);
+            if (distance < nearestDistance) {
+                nearestDistance = distance;
+                nearestHospital = hospital;
+            }
+        }
+        return nearestHospital;
+    }
 }
