cf32480092ea379932e31e7b870c60781db4ec3e
jonathan-julio <55097116+jonathan-julio@users.noreply.github.com>
2023-04-18 20:48:59 -0300

    Update Hospital.java

--- hospital/src/Hospital.java
+++ hospital/src/Hospital.java
@@ -2,6 +2,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Hospital extends Remote {
-    public String getName() throws RemoteException;
-    public Location getLocation() throws RemoteException;
+    String getName() throws RemoteException;
+    String getAddress() throws RemoteException;
+    double getLatitude() throws RemoteException;
+    double getLongitude() throws RemoteException;
 }
