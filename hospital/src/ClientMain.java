2577fc839838c9f19d553ec956a6645d9997d38b
jonathan.j <jonathan.goncalves.703@ufrn.edu.br>
2023-04-19 00:44:32 -0300

    resolvido

--- hospital/src/ClientMain.java
+++ hospital/src/ClientMain.java
@@ -5,16 +5,16 @@ public class ClientMain {
     public static void main(String[] args) {
         try {
             // Obter uma referência ao serviço de ambulância do servidor RMI
-            Object ambulanceService = Naming.lookup("rmi://192.168.1.5:1099/HospitalLocator");
+            AmbulanceServiceInterface hospitalLocator = (AmbulanceServiceInterface) Naming.lookup("rmi://192.168.1.5:1099/HospitalLocator");
 
             // Definir a localização do paciente
-            //Location patientLocation = new Location(52.5167, 13.3833);
+            Location patientLocation = new Location(20.5167, 20.3833);
 
             // Encontrar o hospital mais próximo da localização do paciente
-            //Hospital nearestHospital = ambulanceService.findNearestHospital(patientLocation.getLatitude(), patientLocation.getLongitude() );
+            Hospital nearestHospital = hospitalLocator.findNearestHospital(patientLocation);
 
             // Exibir o resultado
-            System.out.println("O hospital mais próximo é: " + ambulanceService);
+            System.out.println("O hospital mais próximo é: "  + nearestHospital.getName() ) ;
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
