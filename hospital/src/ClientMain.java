7ea42ac45e3e7e60ec53b1bd1cfe13e00a93f1c4
jonathan-julio <jonathan.goncalves.703@ufrn.edu.br>
2023-04-19 00:31:49 -0300

    arq bin

--- hospital/src/ClientMain.java
+++ hospital/src/ClientMain.java
@@ -5,7 +5,7 @@ public class ClientMain {
     public static void main(String[] args) {
         try {
             // Obter uma referência ao serviço de ambulância do servidor RMI
-            AmbulanceService ambulanceService = (AmbulanceService) Naming.lookup("rmi://192.168.1.5:1099/HospitalLocator");
+            Object ambulanceService = Naming.lookup("rmi://192.168.1.5:1099/HospitalLocator");
 
             // Definir a localização do paciente
             //Location patientLocation = new Location(52.5167, 13.3833);
@@ -14,7 +14,7 @@ public class ClientMain {
             //Hospital nearestHospital = ambulanceService.findNearestHospital(patientLocation.getLatitude(), patientLocation.getLongitude() );
 
             // Exibir o resultado
-            System.out.println("O hospital mais próximo é: " );
+            System.out.println("O hospital mais próximo é: " + ambulanceService);
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
