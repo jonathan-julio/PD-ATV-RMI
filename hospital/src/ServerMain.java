ef0ea5b42928517c7f4a3464eeef1e3fdf75f570
jonathan-julio <jonathan.goncalves.703@ufrn.edu.br>
2023-04-18 22:59:33 -0300

    limpando codigo

--- hospital/src/ServerMain.java
+++ hospital/src/ServerMain.java
@@ -6,11 +6,6 @@ import java.rmi.server.UnicastRemoteObject;
 public class ServerMain {
     public static void main(String[] args) {
         try {
-            Hospital[] hospitals = {
-                    new HospitalImpl("Hospital A", "", new Location(10, 20)),
-                    new HospitalImpl("Hospital B","", new Location(30, 40)),
-                    new HospitalImpl("Hospital C","", new Location(50, 60))
-            };
 
             HospitalLocator hospitalLocator = new HospitalLocator();
 
@@ -24,16 +19,3 @@ public class ServerMain {
     }
 }
 
-/* HospitalLocatorImpl hospitalLocator = new HospitalLocatorImpl();
-    List<Hospital> hospitals = new ArrayList<Hospital>();
-    hospitals.add(new Hospital("Hospital 1", new Location(1, 1)));
-    hospitals.add(new Hospital("Hospital 2", new Location(2, 2)));
-    hospitals.add(new Hospital("Hospital 3", new Location(3, 3)));
-    hospitalLocator.setHospitals(hospitals);
-
-    AmbulanceServiceImpl ambulanceService = new AmbulanceServiceImpl(hospitalLocator);
-
-    Registry registry = LocateRegistry.createRegistry(1099);
-    registry.rebind("AmbulanceService", ambulanceService);
-
-    System.out.println("Server is running..."); */
